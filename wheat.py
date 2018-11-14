5f084ead6bcbe431038233c17a3a3b6d0177a9c3
Md Arfinul Haque <mdarfinulhaque@gmail.com>
2018-11-14 17:30:01 +0530

    segmentation of a complete image

--- wheat.py
+++ wheat.py
@@ -4,6 +4,6 @@ from segment_formation import segment_image4
 if __name__ == "__main__":
     imgFile = input("Enter the wheat image path : ")
     # for imgFile in imgFile:
-    print("Segmentation in process...")
+    print("L1 Segmentation in process...")
     segment_image4(imgFile)
     print("Segmentation in Complete.")
